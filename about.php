4132dfbb33f38603d818f8dbc64dc8b4c5c5a7ce
bhaiyaamit <84909297+bhaiyaamit@users.noreply.github.com>
2021-05-27 16:34:41 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About |SES</title>
+		<link href="https://fonts.googleapis.com/css?family=Secular+One" rel="stylesheet"> 
+   <link rel="stylesheet" type="text/css" href="master.css">
+   <style type="text/css">
+   	.content{
+   		max-width: 650px;
+   		border: 5px solid #e1e1e1;
+   		padding: 20px;
+   		text-align: center;
+   		margin-left: auto;
+   		margin-right: auto;
+   	}
+
+   </style>
+</head>
+<body>
+<h1>Online Voting System </h1>
+<div class="content">
+	 <p>Name : Bhaiya Amit</p>
+	 <p>Mentor : Prof. Pritesh vasava</p>
+	 <p>University : GEC Bharuch</p>
+	 <br/>
+	 <br/>
+	 <hr/>
+	 <p>This is the Online Voting System platform build using php,html,css and mysql as a mini-project under mentorship of Prof. P.B.V, GEC.</p>
+
+</div>
+<br>
+<p style="text-align: center; "><a href="index.php" style=" background-color: #000aff; padding: 8px 25px;">Back</a></p>
+</body>
+</html>
